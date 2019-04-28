dc712794853628f136faffd12aefbc5b1cd080bc
Rmaravanyika <rmaravanyika.rm@gmail.com>
2019-04-28 14:59:36 +0200

    fixing bugs

--- blog/models.py
+++ blog/models.py
@@ -42,11 +42,11 @@ class FreeQuote(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    cell_number = models.IntegerField()
-    address = models.CharField(max_length=200)
+    cell_number = models.IntegerField(blank=True)
+    address = models.CharField(max_length=200, blank=True)
     service_req = models.CharField(max_length=4, choices=serviceAvailable )
-    additional_info = models.TextField()
-    loc_from = models.CharField(max_length=200)
+    additional_info = models.TextField(blank=True)
+    location = models.CharField(max_length=200)
      
     def __str__(self):
         return self.name   
