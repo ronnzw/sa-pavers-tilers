1fddf4cc6f2da3da149eb8769687d31a5333279b
Rmaravanyika <rmaravanyika.rm@gmail.com>
2019-05-07 02:44:42 +0200

    added a few more things

--- blog/models.py
+++ blog/models.py
@@ -55,7 +55,7 @@ class FreeQuote(models.Model):
 class Contact(models.Model):
    name = models.CharField(max_length=200)
    email = models.EmailField(max_length=200)
-   phone = models.CharField(max_length=15)
+   phone = models.CharField(max_length=15, blank=True)
    subject = models.CharField(max_length=200)
    message = models.TextField()
 
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.utils import timezone
 
-from .models import Post, FreeQuote
-from .forms import PostForm, FreequoteForm
+from .models import Post, FreeQuote, Newletter
+from .forms import PostForm, FreequoteForm, NewletterForm
 
 def post_list(request):
     form = FreequoteForm(request.POST or None)
@@ -15,17 +15,47 @@ def post_list(request):
     		form = FreequoteForm()
     		success = True
     		messages.success(request, 'Your Free quote request has been submited. Thank you')
-    return render(request, 'home.html', {'form': form})
+
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form = NewletterForm()
+    return render(request, 'home.html', {'form': form, 'sub_form': sub_form})
 
 def about_page(request):
-    return render(request, 'about.html', {})
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form = NewletterForm()  
+    return render(request, 'about.html', {'sub_form': sub_form})
 
 def service_page(request):
-    return render(request, 'service.html', {})
+    form = FreequoteForm(request.POST or None)
+    success =False
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            form = FreequoteForm()
+            success = True
+            messages.success(request, 'Your Free quote request has been submited. Thank you')
+
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form =NewletterForm()                
+    return render(request, 'service.html', {'form': form, 'sub_form': sub_form})
 
 def blogg_page(request):
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form =NewletterForm()    
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blogg.html', {'posts': posts})
+    return render(request, 'blogg.html', {'posts': posts, 'sub_form': sub_form})
 
 def contact_page(request):
     form = PostForm(request.POST or None)
@@ -36,10 +66,23 @@ def contact_page(request):
             form = PostForm()
             success =True
             messages.success(request, 'Your Message has been submitted, our team will be intouch soon.')
-    return render(request, 'contact.html', {'form': form})
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form =NewletterForm()            
+    return render(request, 'contact.html', {'form': form, 'sub_form': sub_form})
 
 def newletter_view(request):
-    return render(request, 'newletter.html', {})
+    form = NewletterForm(request.POST or None)
+    success =False
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            form = NewletterForm()
+            success = True
+            messages.success(request, 'Newletter request has been submitted')    
+    return render(request, 'newletter.html', {'form': form})
 
 def freequote_view(request):
     return render(request, 'freequote.html', {})
